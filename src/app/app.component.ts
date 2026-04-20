0d0c954407c8fd75f82479a56a995dd058bac07a
BalamuruganShanmugam <balamurugan.shanmugam@syncfusion.com>
2026-04-20 19:19:16 +0530

    1021490: Upgrade to angular 19 to resolve dependabot issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToolbarService } from '@syncfusion/ej2-angular-documenteditor';
 @Component({
+standalone: false,
 selector: 'app-root',
 // specifies the template string for the DocumentEditorContainer component
 template: `<ejs-documenteditorcontainer serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" height="600px" style="display:block" [enableToolbar]=true> </ejs-documenteditorcontainer>`,
